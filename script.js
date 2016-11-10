9b36de77c1bcfc2e721823b110670f83884de579
clbarnes <cbarnes@mrc-lmb.cam.ac.uk>
2016-11-10 11:49:20 -0500

    fix rectangle colour

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 var svg = d3.select("body").append("svg").attr("width", 500).attr("height", 500);
 
-svg.append("rect").attr("width", 200).attr("height", 300).attr("x", 0).attr("y", 0).attr("color", "blue");
+svg.append("rect")
+    .attr("width", 200)
+    .attr("height", 300)
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("fill", "red");

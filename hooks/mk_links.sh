324a6fd7d3878eb309821d91ffe7c7751ed4eb68
clbarnes <cbarnes@mrc-lmb.cam.ac.uk>
2016-11-09 19:35:38 -0500

    hooks

--- hooks/mk_links.sh
+++ hooks/mk_links.sh
@@ -2,3 +2,4 @@
 DIR=$(git rev-parse --show-toplevel)
 ln -s $DIR/hooks/pre-commit $DIR/.git/hooks/pre-commit
 ln -s $DIR/hooks/post-commit $DIR/.git/hooks/post-commit
+chmod +x $DIR/.git/hooks/pre-commit $DIR/.git/hooks/post-commit
--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 var svg = d3.select("body").append("svg").attr("width", 500).attr("height", 500);
 
-svg.append("rect").attr("width", 100).attr("height", 100).attr("x", 0).attr("y", 0).attr("color", "blue");
+svg.append("rect").attr("width", 200).attr("height", 300).attr("x", 0).attr("y", 0).attr("color", "blue");
